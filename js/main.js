d0a60f0af389460b56b2de475e9d01635fd515bc
liondot <51257576+liondot@users.noreply.github.com>
2022-08-08 20:20:25 +0900

    getting username

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 const loginForm = document.querySelector("#login_form");
 const loginInput = loginForm.querySelector("input");
+const greeting = document.querySelector("#greeting")
+
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
   event.preventDefault()
   const username = loginInput.value;
-  console.log(loginInput.value)
-  
+  loginForm.classList.add(HIDDEN_CLASSNAME)
+  console.log(username)
+  greeting.innerText = `Hello  ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME)
 }
 
 
