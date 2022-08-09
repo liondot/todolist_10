8c93a0208e4ebea5029a484e420af2b8edc2088c
liondot <51257576+liondot@users.noreply.github.com>
2022-08-09 18:47:05 +0900

    loading username

--- js/main.js
+++ js/main.js
@@ -3,15 +3,32 @@ const loginInput = loginForm.querySelector("input");
 const greeting = document.querySelector("#greeting")
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username"
 
 function onLoginSubmit(event) {
   event.preventDefault()
   const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username)
   loginForm.classList.add(HIDDEN_CLASSNAME)
   console.log(username)
+  paintGreetings(username)
+}
+
+function paintGreetings(username){
   greeting.innerText = `Hello  ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME)
 }
 
 
-loginForm.addEventListener('submit', onLoginSubmit)
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  // show the form 
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener('submit', onLoginSubmit);
+
+} else {
+  // show the greetings
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  paintGreetings(savedUsername);
+}
