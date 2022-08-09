1c1c125306f1d90b948de80fc85399b0f54dda77
liondot <51257576+liondot@users.noreply.github.com>
2022-08-09 21:36:46 +0900

    deleting to button

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,31 @@ const todoForm = document.getElementById("todo_form");
 const todoInput = todoForm.querySelector('input')
 const todoList = document.getElementById("todo_list");
 
+
+function deleteToDo(event){
+  const target = event.target.parentNode;
+  console.log(target)
+  target.remove()
+}
+
+
+function paintToDo(newTodo){
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "X";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
+  todoList.appendChild(li);
+}
+
 function handleToDoSubmit(e){
   e.preventDefault(); 
   const newTodo = todoInput.value;
-  console.log("todoInput.value", todoInput.value)
   todoInput.value = "";
-  console.log("newTodo", newTodo ,":", "todoInput.value", todoInput.value ,)
+  paintToDo(newTodo)
 }
 
 
